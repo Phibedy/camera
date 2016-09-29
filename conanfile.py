f9e422b517e587f05fea3da3d23ca22a26069ae8
phibedy <philipp.schmette@gmx.de>
2016-09-29 13:55:43 +0200

    added .so export

--- conanfile.py
+++ conanfile.py
@@ -15,6 +15,4 @@ class CameraImporterConan(ConanFile):
         self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
-        self.copy("*.h", dst="include",src="include")
-        self.copy("*.lib", dst="lib", src="lib")
-        self.copy("*.a", dst="lib", src="lib")
+        self.copy("*.so",dst="bin")
